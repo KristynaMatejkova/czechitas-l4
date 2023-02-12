b7f180f7cc5edac8242a441b4cfa2d7bc695aa03
LAPTOP-6A2IC92J\rdhd1 <junova.kristyna@gmail.com>
2023-02-12 16:54:53 +0100

    1 a 2úkol

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // tady je místo pro náš program
-//1
+//úkol 1
 let title = "Top Gun";
 console.log(title);
 console.log(title.length);
 console.log(title.toUpperCase());
 console.log(title.slice(0,5));
 console.log(title.slice(-5));
-//2
+//úkol 2
 let email = prompt("Zadejte prosím svou e-mailovou adresu:");
 let atIndex = email.indexOf('@');
 email.slice(0, Number(atIndex));
@@ -21,7 +21,7 @@ document.body.innerHTML = (
 '<p> User name: ' + usersEmail.userName + ' </p>' +
 '<p> Domain: ' + usersEmail.domain + ' </p>'
 );
-//3
+//úkol 3 - zakomentovala jsem, protože přepisuje úkol 2 a až po vytvoření jsem si v classroom všimla, že jsme asi dělat neměli :D
 /*
 let street = prompt("Zadejte prosím název ulice:");
 let houseNumber = prompt("Zadejte prosím číslo domu:");
